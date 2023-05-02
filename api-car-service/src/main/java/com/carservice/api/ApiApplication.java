848dd0e54c6c0a4cd79e0221eae5017d99a1306d
thaisBertoldi <thais_brt@hotmail.com>
2023-05-02 18:22:38 -0300

    correção dependências api principal e criação de microsservico

--- api-car-service/src/main/java/com/carservice/api/ApiApplication.java
+++ api-car-service/src/main/java/com/carservice/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.carservice.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "KafkaTemplate*" })
 public class ApiApplication {
 
 	public static void main(String[] args) {
